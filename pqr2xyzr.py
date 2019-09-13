2597814f6807348418e067cf839728f2cbd46a5a
Artemi Bendandi <37833668+artbendandi@users.noreply.github.com>
2019-09-13 13:46:27 +0200

    Script to transform PDB/PQR files to XYZR files

---
+++ pqr2xyzr.py
@@ -0,0 +1,24 @@
+import sys
+
+pqr_name = sys.argv[1]
+xyzr_name = sys.argv[2]
+xyzr = open(xyzr_name,'w') #generate modified file
+
+pqr_file = open(pqr_name,'r')
+for line in pqr_file:
+    if line[:4] == "ATOM":
+        x = line[30:38]
+        y = line[38:46]
+        z = line[46:54]
+        r = line[63:70]
+        linemod = x + ' ' + y + ' ' + z + ' ' + r + "\n"
+        #print(linemod)
+
+        xyzr.write(linemod)
+    
+
+#Save output
+pqr_file.close()
+xyzr.close()
+
+print("Converted to xyzr")
